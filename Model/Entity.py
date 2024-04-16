203b419012ff1caca87fcc9e9587fe5e130af0bc
Marcelo Biotto <lee.am@live.com>
2024-04-16 07:19:08 -0300

    enviando arquivos iniciais do projeto

---
+++ Model/Entity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+from pygame import Surface
+from pygame import Rect
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.surf Surface = None
+        self.rect Rect = None
+
+    def move(self, ):
+        pass
+
---
+++ Model/Interface1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Interface1(Entity):
+    pass
